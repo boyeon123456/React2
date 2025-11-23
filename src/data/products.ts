d20f66d7e90d88d8b4505fc2cead13e75b0e3bd9
boyeo <boyeon5600@gmail.com>
2025-11-23 16:58:29 +0900

    두번째 커밋

--- src/data/products.ts
+++ src/data/products.ts
@@ -22,5 +22,11 @@ export const products = [
     title: "USB-C 케이블",
     price: 9000,
     img: "C:/Users/boyeo/OneDrive/Desktop/React2/public/ChatGPT Image 2025년 10월 27일 오후 03_44_16.png"
+  },
+  {
+    id: 5,
+    title: "무선 마우스",
+    price: 29000,
+    img: "C:/Users/boyeo/OneDrive/Desktop/React2/public/ChatGPT Image 2025년 10월 27일 오후 03_44_48.png"
   }
 ];
